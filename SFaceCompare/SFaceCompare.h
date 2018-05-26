a2ea7d64441edef674bc9a584463fdfddda085d5
BohdanNikoletti <bohdanrose1@gmail.com>
2018-05-26 15:31:35 +0300

    Initial Commit

---
+++ SFaceCompare/SFaceCompare.h
@@ -0,0 +1,19 @@
+//
+//  SFaceCompare.h
+//  SFaceCompare
+//
+//  Created by Bohdan Mihiliev on 26.05.2018.
+//  Copyright © 2018 Bohdan Mihiliev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SFaceCompare.
+FOUNDATION_EXPORT double SFaceCompareVersionNumber;
+
+//! Project version string for SFaceCompare.
+FOUNDATION_EXPORT const unsigned char SFaceCompareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFaceCompare/PublicHeader.h>
+
+
