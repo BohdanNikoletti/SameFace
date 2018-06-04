bd348fb49d2e34589cbe61f4f0bff23a09f2f60d
Bohdan Mihiliev <bohdanrose1@gmail.com>
2018-06-04 16:46:49 +0300

    Example app

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -10,41 +10,62 @@ import UIKit
 import SFaceCompare
 
 final class ViewController: UIViewController {
-
+  
   // MARK: - Outlets
   @IBOutlet weak private var firstImageView: UIImageView!
   @IBOutlet weak private var secondImageView: UIImageView!
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
   
   // MARK: - Properties
-
   private var images = [UIImage]() {
     didSet {
       if images.count == 2 {
-        CVUtilsInterface.areSameFaces(on: images[0], and: images[1], facesFound: true)
+        activityIndicator.startAnimating()
+        DispatchQueue.global(qos: .background).async {
+          CVUtilsInterface.areSameFaces(on: self.images[0], and: self.images[1], facesFound: true)
+          DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+          }
+        }
       }
     }
   }
+  private var selectedImageViewTag = 0
   
   // MARK: - Lifecycle events
   override func viewDidLoad() {
     super.viewDidLoad()
-    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.connected(_:)))
     
-//    firstImageView.isUserInteractionEnabled = true
-//    firstImageView.tag = 2
-    firstImageView.addGestureRecognizer(tapGestureRecognizer)
+    let firstImageViewTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.connected(_:)))
+    let secondImageViewTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.connected(_:)))
+    
+    firstImageView.addGestureRecognizer(firstImageViewTapGestureRecognizer)
+    secondImageView.addGestureRecognizer(secondImageViewTapGestureRecognizer)
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
-
-
   
-  @objc func connected(_ sender:AnyObject){
-    print("you tap image number : \(sender.view.tag)")
+  override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+    if motion == .motionShake  {
+      print("Did Shake")
+      images.removeAll()
+      firstImageView.image = #imageLiteral(resourceName: "empty-image")
+      secondImageView.image = #imageLiteral(resourceName: "empty-image")
+      firstImageView.isUserInteractionEnabled = true
+      UIView.animate(withDuration: 0.35, animations: {
+        self.secondImageView.alpha = 0.1
+      })
+    }
   }
-
+  
+  // MARK: - Actions
+  @objc func connected( _ sender:AnyObject) {
+    selectedImageViewTag = sender.view.tag
+    presentImagePicker()
+  }
+  
 }
 
 // MARK: - UIImagePickerControllerDelegate
@@ -52,15 +73,30 @@ extension ViewController: UIImagePickerControllerDelegate {
   
   func imagePickerController(_ picker: UIImagePickerController,
                              didFinishPickingMediaWithInfo info: [String : Any]) {
+    
     if let selectedPhoto = info[UIImagePickerControllerOriginalImage] as? UIImage {
       dismiss(animated: true, completion: { [unowned self] in
         self.images.append(selectedPhoto)
+        switch self.selectedImageViewTag {
+        case 0:
+          self.firstImageView.image = selectedPhoto
+          self.secondImageView.isUserInteractionEnabled = true
+          self.firstImageView.isUserInteractionEnabled = false
+          UIView.animate(withDuration: 0.35, animations: {
+            self.secondImageView.alpha = 1
+          })
+        case 1:
+          self.secondImageView.image = selectedPhoto
+          self.secondImageView.isUserInteractionEnabled = false
+        default:
+          fatalError("Unexpected behaviour")
+        }
       })
     }
-  
+    
   }
   
-  @objc func presentImagePicker() {
+  func presentImagePicker() {
     let imagePickerActionSheet = UIAlertController(title: "Snap/Upload Image",
                                                    message: nil, preferredStyle: .actionSheet)
     
@@ -87,7 +123,7 @@ extension ViewController: UIImagePickerControllerDelegate {
     imagePickerActionSheet.addAction(cancelButton)
     present(imagePickerActionSheet, animated: true)
   }
-
+  
 }
 
 // MARK: - UINavigationControllerDelegate
